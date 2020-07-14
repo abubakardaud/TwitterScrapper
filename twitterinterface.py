9a903d858e64c897a8349f64f2148c0ada7340ed
eternalabu <abubakar.daud@mail.mcgill.ca>
2020-07-14 13:41:05 -0400

    Working v1.0,
    Works for single processes perfectly

--- twitterinterface.py
+++ twitterinterface.py
@@ -1,17 +1,36 @@
 import concurrent.futures
+import sys
 import toml
 import time
 import tweepy
 import datetime
 import sqlite3
+import traceback
+from tweepy import RateLimitError, TweepError
+import multiprocessing
 
+# Globals
+counter_var = 0
+
+
+# Load Config
 
 config = toml.load("config.toml")
 hash_tag_array = config["build"]["HASH_TAG"]
 api_key_array = config["build"]["API_KEYS"]
 
+notable_min_followers = config["build"]["notable_min_followers"]
+start_date = config["build"]["start_date"]
+num_requested = config["build"]["num_requested"]
+
+# Intro Script
+
+print("TweetScraper V1.0, Current loaded queries: ")
+print(hash_tag_array)
+
 # Data base config
 
+print("connecting to database . . . ")
 connection = sqlite3.connect("Tweetpy.db")
 crsr = connection.cursor()
 sql_command = """CREATE TABLE Tweets (  
@@ -19,19 +38,38 @@ TweetID INTEGER PRIMARY KEY,
 Handle VARCHAR(100),  
 Created_at VARCHAR(30),  
 Author_loc VARCHAR(200),  
-Text VARCHAR(280));"""
+Text VARCHAR(280),
+Fav_count INTEGER,
+created_time VARCHAR(120),
+Author_id INTEGER);"""
+
+sql_command2 = """CREATE TABLE Authors (  
+AuthorID INTEGER PRIMARY KEY,  
+Name VARCHAR(200),  
+Screen_name VARCHAR(200),
+description VARCHAR(400),  
+Author_loc VARCHAR(300),  
+verified INTEGER,
+followers_count INTEGER,
+friends_count INTEGER,
+favourites_count INTEGER,
+statuses_count INTEGER,
+default_profile VARCHAR(20),
+default_profile_image VARCHAR (20),
+created_at VARCHAR(300));"""
 
 try:
     crsr.execute(sql_command)
 except:
-    print("table already created, old data may be there.")
+    print("Found Old Tweet Database, data may need to be updated.")
 
-min_retweets = str(config["build"]["min_retweets"])
-min_fav = str(config["build"]["min_faves"])
-start_date = config["build"]["start_date"]
+try:
+    crsr.execute(sql_command2)
+except:
+    print("Found Old Author Database, data may need to be updated.")
 
-min_retweets = "min_retweets:" + min_retweets
-min_fav = "min_faves" + min_fav
+connection.commit()
+print("Database connection successful")
 
 
 def auth(consumer_key, consumer_secret, access_token, access_token_secret, number_in_array):
@@ -58,46 +96,71 @@ def auth_array(input_array):
         if (auth_key != None):
             return_array.append(auth_key)
 
-        if (len(return_array) == 0):
+        if len(return_array) == 0:
             print("there are currently no valid authentication keys, please update the config.toml")
             exit(1)
+        print("All Auth keys successfully loaded")
         return return_array
 
 
-def remove_comma(str):
-    str = str.replace(",", ".")
-    return str
-
-
 def limit_handle(cursor):
+    Error_count = 0
     while 1:
         try:
             yield cursor.next()
-        except tweepy.RateLimitError:
+        except TweepError:
             time.sleep(15 * 60)
+            Error_count = Error_count + 1
+            if Error_count == 3:
+                print("\nMax Error Count reached, terminating cycle")
+                return
+        except KeyboardInterrupt:
+            print("User Keyboard Interrupt")
+            print("Cycle ending")
+            print("\n")
+            return
+        except StopIteration:
+            print("\n\nCycle completed successfully . . . \n")
+            return
+
         except:
-            print("Ran out of things to iterate through")
+            traceback.print_exc()
+            print("\n Terminating cycle")
             return
 
 
-def query_create(hashtag):
-    return "#" + hashtag + " " + min_retweets + " " + min_fav
-
-
-def csv_name(hashtag):
-    now = datetime.datetime.now()
-    return hashtag + "_" + now.strftime("%Y-%m-%d_%H-%M") + ".csv"
-
-
-def get_hashtag(auth, hashtag_string):
-
+def get_hashtag(tupple):
+    auth = tupple[0]
+    hashtag_string = tupple[1]
+    global counter_var
+    Tweet_fail = 0
+    print(hashtag_string)
     for tweet in limit_handle(
             tweepy.Cursor(auth.search, q=hashtag_string, lang="en", since=start_date,
-                          tweet_mode="extended").items(10)):
+                          tweet_mode="extended").items(num_requested)):
+        try:
+            crsr.execute("insert into Tweets values(?,?,?,?,?,?,?,?)", (
+            tweet.id, tweet.author.screen_name, tweet.created_at, tweet.author.location, tweet.full_text,
+            tweet.favorite_count, tweet.created_at, tweet.user.id))
+        except:
+            Tweet_fail = Tweet_fail + 1
 
-        crsr.execute("insert into Tweets values(?,?,?,?,?)",(tweet.id_str,tweet.author.screen_name,tweet.created_at,tweet.author.location,tweet.full_text))
-        print(tweet.author.screen_name)
+        try:
+            crsr.execute("insert into Authors values(?,?,?,?,?,?,?,?,?,?,?,?,?)", (
+                tweet.user.id, tweet.user.name, tweet.user.screen_name, tweet.user.description, tweet.user.location,
+                tweet.user.verified, tweet.user.followers_count, tweet.user.friends_count, tweet.user.favourites_count,
+                tweet.user.statuses_count, tweet.user.default_profile, tweet.user.default_profile_image,
+                tweet.user.created_at))
+        except:
+            Tweet_fail = Tweet_fail + 1
 
+        counter_var = counter_var + 1
+        sys.stdout.write("\rNum Tweets Counted = %i" % counter_var)
+        sys.stdout.flush()
+
+        if counter_var % 100 == 1:
+            connection.commit()
+    connection.commit()
 
 
 # + "," + tweet.author.screen_name + "," + tweet.user.id_str + "," + tweet.created_at + "," + tweet.author.location + "," + str(tweet.retweet_count) + "," + str(tweet.favorite_count) + "," + tweet.full_text + "\n"
@@ -106,26 +169,18 @@ def get_hashtag(auth, hashtag_string):
 if __name__ == "__main__":
 
     authentication_ids = auth_array(api_key_array)
+    '''
+    pool_array = []
+    counter = 0
+    for i in hash_tag_array:
+        Holding_Tupple = [authentication_ids[counter % len(authentication_ids)], i]
+        pool_array.append(Holding_Tupple)
+        counter = counter + 1
 
-    holding_for_processes = []
-    i = 0
-    for x in hash_tag_array:
-        holding_for_processes.append([x, authentication_ids[i % len(authentication_ids)]])
-        i = i + 1
+    pool = multiprocessing.Pool()
+    results = pool.map(get_hashtag,pool_array)
+    '''
 
-    get_hashtag(authentication_ids[0], "blm")
+    get_hashtag([authentication_ids[0],hash_tag_array[0]])
+    connection.commit()
 
-crsr.execute("SELECT * FROM Tweets")
-ans = crsr.fetchall()
-print(ans)
-connection.commit()
-
-
-#    with concurrent.futures.ThreadPoolExecutor() as executor:
-#        processes = []
-
- #       for l in holding_for_processes:
- #           processes.append(executor.submit(get_hashtag, l[1], l[0]))
-
-  #      for f in concurrent.futures.as_completed(processes):
-  #         print("Completed a process")
